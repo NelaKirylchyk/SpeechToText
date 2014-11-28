c18f2d4751e4f177af3c0a1541e02be74662893b
Nella Kirilchik <Nela_Kirylchyk@epam.com>
2014-11-28 11:34:31 +0300

    Speech to text recognition

---
+++ SpeechToTextApp/SpeechToTextApp.Windows/MainPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Bing.Speech;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SpeechToTextApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private SpeechRecognizer SR;
+        private const string LanguageTag = "en-US";
+        private readonly List<string> speechCache = new List<string>(8);
+        private SpeechRecognizerAudioCaptureState lastState;
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Inititalize SpeechRecognizer with ADM credentials.
+            SR = new SpeechRecognizer(LanguageTag, new SpeechAuthorizationParameters()
+            {
+                ClientId = "speechToTextApp",
+                ClientSecret = "Z6VMUYLWZcUklsEE9PhOpYA2viJJdxikCotQk2YUSaM="
+            });
+            SR.AudioCaptureStateChanged += SR_AudioCaptureStateChanged;
+            SR.RecognizerResultReceived += SR_RecognizerResultReceived;
+
+        }
+
+        /// <summary>
+        /// Starts a speech recognition session through the custom UI.
+        /// </summary>
+        private async void SpeakButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Always call RecognizeSpeechToTextAsync from inside
+            // a try block because it calls a web service.
+            try
+            {
+                while (true)
+                {
+                    SpeechRecognitionResult result = await SR.RecognizeSpeechToTextAsync();
+
+                    // Write the result to the TextBlock.
+                    if (result.TextConfidence != SpeechRecognitionConfidence.Rejected)
+                    {
+                        speechCache.Add(result.Text);
+                        ResultText.Text = string.Join("\n", speechCache);
+                    }
+                    else
+                    {
+                        ResultText.Text = "Sorry, I did not get that.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // If there's an exception, show the Type and Message.
+                ResultText.Text = string.Format("{0}: {1}", ex.GetType(), ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Update the speech recognition audio capture state.
+        /// </summary>
+        private async void SR_AudioCaptureStateChanged(SpeechRecognizer sender, SpeechRecognitionAudioCaptureStateChangedEventArgs args)
+        {
+            Debug.WriteLine(args.State);
+            lastState = args.State;
+            switch (args.State)
+            {
+                case SpeechRecognizerAudioCaptureState.Canceled:
+                    Status.Text = "Operation cancelled.";
+                    break;
+                case SpeechRecognizerAudioCaptureState.Cancelling:
+                    Status.Text = "Cancelling capture operation...";
+                    break;
+                case SpeechRecognizerAudioCaptureState.Complete:
+                    Status.Text = "Audio capture complete.";
+                    break;
+                case SpeechRecognizerAudioCaptureState.Initializing:
+                    Status.Text = "Initializing audio capture...";
+                    break;
+                case SpeechRecognizerAudioCaptureState.Listening:
+                    Status.Text = "Listening...";
+                    break;
+                case SpeechRecognizerAudioCaptureState.Thinking:
+                    Status.Text = "Interpreting audio input...";
+                    break;
+                default:
+                    Status.Text = "Unknown capture state.";
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// A result was received. Whether or not it is intermediary depends on the capture state.
+        /// </summary>
+        private void SR_RecognizerResultReceived(SpeechRecognizer sender, SpeechRecognitionResultReceivedEventArgs args)
+        {
+            Debug.WriteLine(args.Text);
+            if (args.Text != null)
+            {
+                if (lastState != SpeechRecognizerAudioCaptureState.Thinking)
+                    ResultText.Text = String.Join("\n", speechCache.Concat(Enumerable.Repeat(args.Text, 1)));
+            }
+        }
+
+        private void StopListeningButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //SR.StopListeningAndProcessAudio();
+            SR.RequestCancelOperation();
+        }
+    }
+}
---
+++ SpeechToTextApp/SpeechToTextApp.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.Media.SpeechRecognition;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SpeechToTextApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly SpeechRecognizer _speechRecognizer;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Required;
+
+            _speechRecognizer = new SpeechRecognizer();
+            _speechRecognizer.StateChanged += SpeechRecognizerOnStateChanged;
+            _speechRecognizer.RecognitionQualityDegrading += SpeechRecognizerOnRecognitionQualityDegrading;
+
+            // Create an instance of SpeechRecognizer.
+            var topicConstraint = new SpeechRecognitionTopicConstraint(SpeechRecognitionScenario.WebSearch, "webSearch");
+            _speechRecognizer.Constraints.Add(topicConstraint);
+
+            // Compile the dictation grammar by default.
+            _speechRecognizer.CompileConstraintsAsync();
+        }
+
+        private async void SpeechRecognizerOnRecognitionQualityDegrading(SpeechRecognizer sender, SpeechRecognitionQualityDegradingEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(
+                       () =>
+                       {
+                           Problem.Text = args.Problem.ToString();
+                       }));
+        }
+
+        private async void SpeechRecognizerOnStateChanged(SpeechRecognizer sender, SpeechRecognizerStateChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(
+                    () =>
+                    {
+                        Status.Text = args.State.ToString();
+                    }));
+        }
+
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Start recognition.
+            IAsyncOperation<SpeechRecognitionResult> speechRecognitionResult = _speechRecognizer.RecognizeAsync();
+            //IAsyncOperation<SpeechRecognitionResult> speechRecognitionResult = _speechRecognizer.RecognizeWithUIAsync();
+            speechRecognitionResult.Completed += Completed;
+        }
+
+        private async void Completed(IAsyncOperation<SpeechRecognitionResult> asyncInfo, AsyncStatus asyncStatus)
+        {
+            try
+            {
+                var results = asyncInfo.GetResults();
+
+                if (results.Confidence != SpeechRecognitionConfidence.Rejected)
+                {
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(
+                        () =>
+                        {
+                            Result.Text = results.Text;
+                        }));
+                    //var messageDialog = new Windows.UI.Popups.MessageDialog(results.Text, "Text spoken");
+                    //await messageDialog.ShowAsync();
+                }
+                else
+                {
+                    Result.Text = "Sorry, I did not get that.";
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
